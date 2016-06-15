011229f460269fdd10f0e7a966bbb1f0896c2f23
Patryk <patr15@vp.pl>
2016-06-15 11:27:07 +0200

    handling actions

--- vCopernicus-device/vcopernicus_device/templates/node/home/telegraph.py
+++ vCopernicus-device/vcopernicus_device/templates/node/home/telegraph.py
@@ -1,5 +1,7 @@
 from time import time
 from threading import Timer
+import json
+import requests
 
 class Telegraph:
 
@@ -8,6 +10,7 @@ class Telegraph:
         self.mqttc = mqttc
         self.on_recv_msg = on_recv_msg
         mqttc.on_message = self.on_message
+        mqttc.subscribe("actions/1567231", 0)
         self.actions = [{},{},{},{}]
         self.channel = 0
         self.clicks = -1
@@ -15,14 +18,25 @@ class Telegraph:
 
     def on_message(self, mqttc, obj, msg):
         print 'received message from topic ' + msg.topic
-        if msg.topic != self.channel_name: return
-        self.on_recv_msg(msg.payload)      
+        if msg.topic=='actions/1567231':
+            print 'payload: ' + msg.payload
+            body = json.loads(msg.payload)
+            self.actions = [{},{},{},{}]
+            print 'body: ' + str(body)
+            for item in body:
+                print 'item: ' + str(item)
+                self.set_action(item.click, item.channel, {event: item.eventName, data: item.customData})
 
     def perform_action(self, action):
         print 'performing action ...'
+        json_body = json.dumps({value1: action.data})
+        url = 'https://maker.ifttt.com/trigger/{}/with/key/{{key}}'.format(action.event)
+        r = requests.post(url, json=json_body)
 
     def set_action(self, click_num, channel, action):
         self.actions[channel][click_num] = action
+        print 'action set for: ' + str(click_num) + ' clicks on channel ' + \
+        str(channel) + ' action: ' + str(action)
 
     def change_channel(self, channel_num):
         if self.channel==channel_num: return
@@ -51,6 +65,10 @@ class Telegraph:
         print 'release'
         self.clicks+=1
         return 0
+
+    def next_channel(self):
+        self.change_channel((self.channel+1)%4)
+        return self.channel
 
     def button_state(self, state):
         if(state): self.button_press()
